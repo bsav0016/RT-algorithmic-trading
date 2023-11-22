7fb7913d9dd56098b87f9ef4275ad797161e2ceb
Brian Savidge <bsav0016@gmail.com>
2023-11-21 17:09:43 -1000

    More additions

--- util.py
+++ util.py
@@ -35,7 +35,7 @@ def get_base_url():
 def get_crypto_data(symbol="BTC/USD", time_frame=TimeFrame.Day, start_date=dt.datetime(2020, 1, 1), end_date=dt.datetime(2022, 1, 1)):
     client = CryptoHistoricalDataClient()
 
-    print(start_date, end_date)
+    print(type(start_date), type(end_date))
 
     request_params = CryptoBarsRequest(
                         symbol_or_symbols=[symbol],
